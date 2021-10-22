329826d33ec7c0c1b556441f4d361c56182f344f
Juventino Aguilar Correa <superjuve@outlook.es>
2021-10-22 12:57:09 -0500

    Cambio de multiplos

--- multiplos.py
+++ multiplos.py
@@ -3,7 +3,7 @@ numeroCiclos = int(input())
 print("Multiplos:")
 
 for i in range(1,numeroCiclos+1):
-	print((i*2)+"Este es un multiplo")
+	print((i*9)+"Este es un multiplo nuevee")
 	print("Bienvenidos al taller, los quiero mucho <3")
 	print("\t",multiploDos)
 	
